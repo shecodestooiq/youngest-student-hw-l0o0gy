9692b4d540160df4ee297aa10eb356b8bc62857b
l0o0gy <minamahone9620@gmail.com>
2024-02-19 04:24:51 +0300

    show the youngest

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function showYoungestStudent(students) { }
-
-module.exports = showYoungestStudent;
+function showYoungestStudent(students) { 
+    let youngestStudent=students[0];
+    for (let i = 0; i< students.length; i++) {
+        if (students[i].age < youngestStudent.age) {
+            youngestStudent = students[i];   
+        }
+    }
+    console.log(youngestStudent.name);
+}
+var students= [
+    { name: 'Alice', age: 25 },
+    { name: 'Bob', age: 2 },
+    { name: 'Charlie', age: 21 },
+];
+showYoungestStudent(students);
